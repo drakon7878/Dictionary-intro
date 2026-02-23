9af8de3a42c681b04b2c7a9938261aed7ba53794
drakon7878 <jaivin7878@gmail.com>
2026-02-23 17:40:08 +0530

    Add files via upload

--- goodshot game.py
+++ goodshot game.py
@@ -1,19 +1,31 @@
-import pgzero
+import os
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+import pgzrun
+import pgzero.screen
+import pgzero.actor
+import pgzero.rect
+
+screen: pgzero.screen.Screen
+Actor = pgzero.actor.Actor
+Rect = pgzero.rect.Rect
+
 from random import randint
 
-title = "Good Shot"
+TITLE = "Good Shot"
 WIDTH = 500
 HEIGHT = 500
 message = ""
-alien = pgzero.actor('alien')
+count = 0
+alien = Actor("alien")
 alien.pos = 50,50
 
 def draw():
-    pgzero.screen.clear()
-    pgzero.screen.fill(color = (100,0,100))
+    screen.clear()
+    screen.fill(color = (100,0,100))
     place_alien()
     alien.draw()
-    pgzero.screen.draw.text(message, centre = (400,10), fontsize = 30)
+    screen.draw.text(message + " " + str(count) if count !=0 else message +"", center = (400,10), fontsize = 30)
 
 
 def place_alien():
@@ -21,14 +33,15 @@ def place_alien():
     alien.y = randint(50, HEIGHT-50)
 
 def on_mouse_down(pos):
-    global message
+    global message, count
 
     if alien.collidepoint(pos):
         message = "Good Shot"
+        count+=1
         place_alien()
     else:
         message = "You missed"
     
 place_alien()
 
-pgzero.runner()
+pgzrun.go()
