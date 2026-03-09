42cdf731367fecdf3e6293db65759042d568f217
drakon7878 <jaivin7878@gmail.com>
2026-03-09 18:27:51 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,9 @@
+Student = ("Jaivin" , 14)
+address = ("plotno" , [1,9,6], [1,9,7] , ["baba", "budha", "avenue"] , "Amritsar" )
+
+print(address[0][5])
+print(address[1][2])
+print(address[2][1])
+print(address[3][2])
+
+
